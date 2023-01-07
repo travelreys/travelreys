c0503a1a39a4e9bb70ef88a6623ffc15397426dc
awhdesmond <awhdes@gmail.com>
2023-01-07 21:38:17 +0800

    refactor navbar

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react';
+import Link from 'next/link';
+
+import { GlobeAmericasIcon } from '@heroicons/react/24/solid'
 
 const NavBar: FC = () => {
   return (
-    <div className="container flex mb-1 px-56 py-5">
-      Navbar
+    <div className="container flex mb-1 px-24 py-5">
+      <h1 className="text-3xl font-bold text-indigo-500">
+         <Link href="/">
+            <GlobeAmericasIcon className='inline align-bottom h-8 w-8'/>
+            <span className='inline-block pl-1'>tiinyplanet</span>
+          </Link>
+      </h1>
     </div>
   );
 }
