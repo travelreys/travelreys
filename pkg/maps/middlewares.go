393e8e420f4c714f19530800f2f73cec6fd21079
awhdesmond <awhdes@gmail.com>
2023-03-09 20:01:38 +0800

    add locale support for maps

--- pkg/maps/middlewares.go
+++ pkg/maps/middlewares.go
@@ -22,20 +22,20 @@ func ServiceWithRBACMiddleware(svc Service, logger *zap.Logger) Service {
 	return &rbacMiddleware{svc, logger}
 }
 
-func (mw rbacMiddleware) PlacesAutocomplete(ctx context.Context, query, types, sessiontoken string) (AutocompletePredictionList, error) {
+func (mw rbacMiddleware) PlacesAutocomplete(ctx context.Context, query, types, sessiontoken, lang string) (AutocompletePredictionList, error) {
 	ci, err := reqctx.ClientInfoFromCtx(ctx)
 	if err != nil || ci.HasEmptyID() {
 		return AutocompletePredictionList{}, ErrRBACMissing
 	}
-	return mw.next.PlacesAutocomplete(ctx, query, types, sessiontoken)
+	return mw.next.PlacesAutocomplete(ctx, query, types, sessiontoken, lang)
 }
 
-func (mw rbacMiddleware) PlaceDetails(ctx context.Context, placeID string, fields []string, sessiontoken string) (Place, error) {
+func (mw rbacMiddleware) PlaceDetails(ctx context.Context, placeID string, fields []string, sessiontoken, lang string) (Place, error) {
 	ci, err := reqctx.ClientInfoFromCtx(ctx)
 	if err != nil || ci.HasEmptyID() {
 		return Place{}, ErrRBACMissing
 	}
-	return mw.next.PlaceDetails(ctx, placeID, fields, sessiontoken)
+	return mw.next.PlaceDetails(ctx, placeID, fields, sessiontoken, lang)
 }
 
 func (mw rbacMiddleware) Directions(ctx context.Context, originPlaceID, destPlaceID, mode string) (RouteList, error) {
