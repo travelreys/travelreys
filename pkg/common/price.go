c75ab6af062ef9502f57d0326f4e4382659283e6
awhdesmond <awhdes@gmail.com>
2023-04-25 20:15:26 +0800

    change price to priceitem

---
+++ pkg/common/price.go
@@ -0,0 +1,28 @@
+package common
+
+const (
+	PriceSplitMethodSolo       = "solo"
+	PriceSplitMethodAbsolute   = "absolute"
+	PriceSplitMethodPercentage = "percentage"
+)
+
+type PriceSplitTarget struct {
+	ID      string  `json:"id" bson:"id"`
+	Value   float64 `json:"value" bson:"value"`
+	Settled bool    `json:"settled" bson:"settled"`
+}
+
+type PriceSplitOptions struct {
+	Method  string                      `json:"method" bson:"method"`
+	Targets map[string]PriceSplitTarget `json:"targets" bson:"targets"`
+}
+
+type Price struct {
+	Amount   float64 `json:"amount" bson:"amount"`
+	Currency string  `json:"currency" bson:"currency"`
+}
+
+type PriceItem struct {
+	Price
+	SplitOptions PriceSplitOptions `json:"splitOptions" bson:"splitOptions"`
+}
