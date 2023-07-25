de90e154f3ed5549a073497812771ee7ce849bec
awhdesmond <awhdes@gmail.com>
2023-07-25 20:37:43 +0800

    dns lookup ogp

--- pkg/ogp/middlewares.go
+++ pkg/ogp/middlewares.go
@@ -3,22 +3,49 @@ package ogp
 import (
 	"context"
 	"errors"
+	"net"
 
 	"github.com/travelreys/travelreys/pkg/reqctx"
 	"go.uber.org/zap"
 )
 
 var (
-	ErrRBAC = errors.New("ogp.ErrRBAC")
+	ErrValidation = errors.New("ogp.ErrValidation")
+	ErrRBAC       = errors.New("ogp.ErrRBAC")
 )
+
+type validationMiddleware struct {
+	next   Service
+	logger *zap.Logger
+}
+
+func SvcWithValidation(svc Service, logger *zap.Logger) Service {
+	return &validationMiddleware{svc, logger.Named("ogp.validationMiddleware")}
+}
+
+func (mw validationMiddleware) Fetch(ctx context.Context, url string) (Opengraph, error) {
+	if url == "" {
+		return Opengraph{}, ErrValidation
+	}
+	ips, err := net.LookupIP(url)
+	if err != nil {
+		return Opengraph{}, ErrValidation
+	}
+	for _, ip := range ips {
+		if ip.IsPrivate() {
+			return Opengraph{}, ErrValidation
+		}
+	}
+	return mw.next.Fetch(ctx, url)
+}
 
 type rbacMiddleware struct {
 	next   Service
 	logger *zap.Logger
 }
 
-func ServiceWithRBACMiddleware(svc Service, logger *zap.Logger) Service {
-	return &rbacMiddleware{svc, logger}
+func SvcWithRBACMw(svc Service, logger *zap.Logger) Service {
+	return &rbacMiddleware{svc, logger.Named("ogp.rbacMiddleware")}
 }
 
 func (mw rbacMiddleware) Fetch(ctx context.Context, url string) (Opengraph, error) {
