6853eee7ec5adaa8f83aa74be4dc4419f100aeb3
awhdesmond <awhdes@gmail.com>
2023-08-15 21:48:22 -0700

    some auth refactoring

--- pkg/auth/social.go
+++ pkg/auth/social.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 
+	"net/http"
 	"net/url"
 	"os"
 
@@ -103,4 +104,45 @@ func (gp *GoogleProvider) TokenToUserInfo(ctx context.Context, code string) (Goo
 	gusr := GoogleUser{}
 	err = json.Unmarshal(data, &gusr)
 	return gusr, err
+}
+
+const (
+	fbGraphUserURL = "https://graph.facebook.com/me"
+)
+
+type FacebookProvider struct{}
+
+func NewFacebookProvider() FacebookProvider {
+	return FacebookProvider{}
+}
+
+func (fb FacebookProvider) Get(getURL *url.URL) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, getURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	return body, err
+}
+
+func (fb FacebookProvider) TokenToUserInfo(ctx context.Context, code string) (FacebookUser, error) {
+	queryURL, _ := url.Parse(fbGraphUserURL)
+	queryParams := queryURL.Query()
+	queryParams.Set("fields", "id,name,email,picture")
+	queryParams.Set("access_token", code)
+	queryURL.RawQuery = queryParams.Encode()
+
+	body, err := fb.Get(queryURL)
+	if err != nil {
+		return FacebookUser{}, err
+	}
+
+	fbUsr := FacebookUser{}
+	err = json.Unmarshal(body, &fbUsr)
+	return fbUsr, err
 }
